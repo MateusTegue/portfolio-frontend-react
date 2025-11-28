dbb71713f47e847b71c5a56c877abd9b100e824b
mateusTegue <teusteguetorres2001@gmail.com>
2025-11-27 23:16:04 -0500

    feat: Add Vercel configuration and update Vite config for history API fallback

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-//import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-//export default defineConfig({
-//  plugins: [react()],
-//})
-
 import { defineConfig } from 'vite'
 import path from 'path'
 
@@ -14,5 +6,11 @@ export default defineConfig({
     alias: {
       'tailwindcss/version.js': path.resolve(__dirname, 'node_modules/tailwindcss/package.json')
     }
+  },
+  server: {
+    historyApiFallback: true
+  },
+  preview: {
+    historyApiFallback: true
   }
 })
